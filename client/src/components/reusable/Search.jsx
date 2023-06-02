0912650326ee1aac4199066fb8527211e876bda9
Caroline Peake <peake.caroline@gmail.com>
2023-06-02 11:55:40 -0700

    colored background of search icon when hovered

--- client/src/components/reusable/Search.jsx
+++ client/src/components/reusable/Search.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 
-function Search({ setSearchTerm, searchTerm, placeholder, searchClosed, setSearchClosed, clickHandler, theme }) {
+function Search({
+  searchTerm,
+  setSearchTerm,
+  placeholder,
+  searchClosed,
+  clickHandler,
+}) {
   return (
     <QuestionSearchBar searchClosed={searchClosed}>
       <Input
@@ -11,99 +18,86 @@ function Search({ setSearchTerm, searchTerm, placeholder, searchClosed, setSearc
         setSearchTerm={setSearchTerm}
         searchClosed={searchClosed}
         onChange={(event) => setSearchTerm(event.target.value)}
-        theme={theme}
       />
       <SearchIcon type="submit" onClick={clickHandler} searchClosed={searchClosed} />
     </QuestionSearchBar>
   );
 }
+
+Search.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  setSearchTerm: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  searchClosed: PropTypes.bool,
+  clickHandler: PropTypes.func.isRequired,
+};
+
+Search.defaultProps = {
+  placeholder: 'Search...',
+  searchClosed: false,
+};
+
 const QuestionSearchBar = styled.form`
   display: flex;
   justify-content: space-between;
   padding: 2px;
   border: 1px solid currentColor;
   border-radius: 5px;
-  color: #555;
-  background-color: ${(props) => props.theme.backgroundColor};
   width: 100%;
-
-  @medi (min-width: 700px) AND (max-width: 1000px) {
-    ${(props) => !props.searchClosed && css`
-      max-width: 150px;
-      width: 50%;
+  font-size: clamp(0.875rem, calc(0.875rem + 0.268vw), 1.25rem);
+  background-color: ${(props) => props.theme.backgroundColor};
+  @media (max-width: 65em) {
+    ${(props) => props.searchClosed && css`
+      border: 1px solid transparent;
+      background-color: ${props.theme.navBgColor};
     `};
   };
-
-  @media (max-width: 1000px) {
-    ${(props) => props.searchClosed && css`
-    border: none;
-    background-color: ${props.theme.navColor};
-    font-size: ${props.theme.input};
-    padding: 0px;
-  `};
-  };
 `;
-// font-size: 14px;
-// margin: 0.5rem;
 
 // TO-DO: make bigger on focus (for mobile)
 const Input = styled.input`
-  border: none;
+  font-family: inherit;
   background: transparent;
   margin: 0;
-  padding: 7px 8px;
+  padding: 0.5em;
   font-size: ${(props) => props.theme.input};
-  color: inherit;
+  color: ${(props) => props.theme.fontColor};
   border: 1px solid transparent;
   border-radius: inherit;
-  width: 95%;
- ::placeholder {
-   color: ${(props) => props.theme.inputPlaceholder};
+  flex: 1;
+  width: 100%;
+  ::placeholder {
+    color: ${(props) => props.theme.inputPlaceholder};
   };
 
-  @media (max-width: 1000px) {
+  @media (max-width: 65em) {
     ${(props) => props.searchClosed && css`
-      width: 0;
+      width: 0px;
+      border: none;
       padding: 0px;
     `};
   };
 `;
-// font-size: 14px;
-// ${(props) => !props.searchClosed && css`
-// width: 0;
-// padding: 3.5px 4px;
-// font-size: 12px;
-// `};
-// font-size: calc(10px + 1vw);
 
 const SearchIcon = styled.button`
-  text-indent: -999px;
-  overflow: hidden;
-  width: 40px;
-  padding: 0;
+  width: 2em;
+  padding: 1em;
   margin: 0;
+  margin-left: 2px;
   border: 1px solid transparent;
   border-radius: inherit;
   background: transparent url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E") no-repeat center;
   cursor: pointer;
-  opacity: 0.7;
+  color: ${(props) => props.theme.navFontColor};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  aspect-ratio: 1;
   &:hover {
-    opacity: 1;
-    background-color: ${(props) => props.theme.backgroundColor};
-  }
-
-  @media (max-width: 1000px) {
-    ${(props) => props.searchClosed && css`
-      font-size: ${props.theme.input};
-      display: flex;
-      height: 25px;
-      flex-direction: column;
-      justify-content: flex-end;
-      align-items: flex-end;
-      border: none;
-    `};
+    color: ${(props) => props.theme.navActiveFontColor};
+    background-color: ${(props) => props.theme.iconHoverBackgroundColor};
   };
 `;
-// font-size: 14px;
 
 export default Search;
