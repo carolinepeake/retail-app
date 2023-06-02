8a33f5aad72d3fa9ec667ee0b23154188d702225
Caroline Peake <peake.caroline@gmail.com>
2023-06-02 11:51:36 -0700

    added uniform color to nav bar, fixed width of responsive search bar, made height of search bar and nav consistent

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,6 +4,8 @@ import styled, { css } from 'styled-components';
 import Search from './reusable/Search';
 import Button from './reusable/Button';
 
+//TO-DO: add scroll event listener for nav sections
+
 function NavBar({ toggleTheme }) {
   NavBar.propTypes = {
     toggleTheme: PropTypes.func.isRequired,
@@ -49,13 +51,20 @@ function NavBar({ toggleTheme }) {
           key={link.label}
           data-target={link.target}
           onClick={(event) => scrollTo(event)}
+          // href={`#${link.target}`}
         >
           {link.label}
         </GridItem>
       ))}
 
       <RightSide searchClosed={searchClosed}>
-        <Search placeholder="Search..." searchTerm={searchTerm} setSearchTerm={setSearchTerm} searchClosed={searchClosed} setSearchClosed={setSearchClosed} clickHandler={clickHandler} theme={{ input: '14px' }} />
+        <Search
+          placeholder="Search..."
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          searchClosed={searchClosed}
+          clickHandler={clickHandler}
+        />
 
         <CollapsedNav
           type="button"
@@ -83,6 +92,7 @@ function NavBar({ toggleTheme }) {
               secondary
               i={i}
               navLinksLength={navLinks.length}
+              // href={`#${link.target}`}
             >
               {link.label}
             </GridItem>
@@ -96,15 +106,12 @@ function NavBar({ toggleTheme }) {
   );
 }
 
-// navColor: '#3a4b53',
-// navBarFont: '#fff',
-
 const Background = styled.div`
-  z-index: 5;
+  z-index: 50;
   position: sticky;
   top: 0;
-  background-color: ${(props) => props.theme.navColor};
-  color: ${(props) => props.theme.navBarFont};
+  background-color: ${(props) => props.theme.navBgColor};
+  color: ${(props) => props.theme.navFontColor};
   background-size: 100% 100%;
   background-repeat: no-repeat;
   height: auto;
@@ -112,189 +119,152 @@ const Background = styled.div`
   justify-content: space-between;
   align-content: center;
   align-items: center;
-
   font-weight: 700;
   font-stretch: ultra-condensed;
+  font-size: clamp(0.875rem, calc(0.5rem + 0.75vw), 1.25rem);
+  padding: 0.5em 5%;
 
-  padding: 0.5rem 5%;
-
-  @media (min-width: 700px) {
-    padding: 0.75rem 2.5%;
+  @media (min-width: 50rem) {
+    padding: 0.75em 2.5%;
     flex-wrap: ${(props) => (props.searchClosed ? 'nowrap' : 'wrap')};
   };
 
-  @media (min-width: 800px) {
-    flex-wrap: wrap;
-  };
-
-  @media (min-width: 1081px) {
+  @media (min-width: 57rem) {
     flex-wrap: nowrap;
-    padding: 0.75rem 5%;
   };
 
+  @media (min-width: 62rem) {
+    padding: 0.75em 5%;
+  };
 `;
-
-// @media (min-width: 1044px) {
-//   padding: 0.75rem 5%;
-// };
 
 const Logo = styled(Button)`
     border: none;
     border-radius: 50px;
-    padding: 0px calc(4px + 0.25vw);
     margin: 0;
-    font-size: calc(24px + 1.2vw);
+    background-color: ${(props) => props.theme.backgroundColor};
+    color:  ${(props) => props.theme.navFontColor};
     &:hover {
-      background-color: ${(props) => props.theme.submitButton};
-      color: ${(props) => props.theme.submitButtonFont};
+      background-color: ${(props) => props.theme.secondaryBackgroundColor};
+      color: ${(props) => props.theme.navActiveFontColor};
       box-shadow: initial;
     };
-    background-color: ${(props) => props.theme.submitButtonHoverFont};
-    color:  ${(props) => props.theme.submitButtonHover};
+    font-size: calc(24px + 1.2vw);
+    padding: 0px calc(4px + 0.25vw);
 
     @media (min-width: 900px) {
       padding: calc(1px + 0.5vw) calc(4px + 0.5vw);
       font-size: calc(10px + 1.2vw);
     };
 `;
-// font-size: calc(28px + 1.2vw);
-// margin: initial;
 
-// breakpoint was previously 992 px
 const GridItem = styled.div`
-  @media (max-width: 700px) {
+  @media (max-width: 50rem) {
     display: ${(props) => (props.isExpanded ? 'block' : 'none')};
   };
   display: flex;
   align-items: center;
   justify-content: space-evenly;
   width: fit-content;
+  padding: 0 0.25em;
   cursor: pointer;
   &:hover {
     text-decoration: underline;
+    color: ${(props) => props.theme.navActiveFontColor};
   };
   &:visited {
-    text-decoration: ${(props) => props.theme.visitedColor}
+    color: ${(props) => props.theme.visitedColor};
   };
-  font-size: calc(8px + .75vw);
-  padding: 0 0.25em;
+
   ${(props) => props.secondary && css`
-    font-size: calc(10px + 1vw);
+    font-size: calc(0.625em + 1vw);
     border-top: grey solid 1px;
-    border-right: black solid 1px;
-    border-left: black solid 1px;
+    border-right: ${props.theme.fontColor} solid 1px;
+    border-left: ${props.theme.fontColor} solid 1px;
     box-sizing: border-box;
     padding: 1em;
     width: 100%;
     text-align: left;
-    background-color: ${props.theme.navColor};
+    background-color: ${props.theme.navBgColor};
     transition: transform 0.25s ease;
     &:hover {
       transform: scale(1.025);
       background-color: ${props.theme.submitButton};
       text-decoration: none;
-      border: black solid 1px;
+      border: ${props.theme.fontColor} solid 1px;
     };
   `};
 `;
 
-// const GridItem = styled.div`
-//   @media (max-width: 700px) {
-//     display: ${(props) => (props.isExpanded ? 'block' : 'none')};
-//     &:hover {
-//       transform: scale(1.025);
-//     };
-//   };
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-evenly;
-//   width: fit-content;
-//   cursor: pointer;
-//   &:hover {
-//     text-decoration: underline;
-//   };
-//   &:visited {
-//     text-decoration: ${(props) => props.theme.visitedColor}
-//   };
-//   font-size: calc(8px + .75vw);
-//   padding: 0 0.25em;
-//   ${(props) => props.secondary && css`
-//     font-size: calc(10px + 1vw);
-//     border-bottom: currentColor solid 2px;
-//     padding: 5%;
-//     background-color: ${props.theme.navColor};
-//     transition: transform 0.25s ease;
-//     border-radius: ${props.i === props.navLinksLength - 1 && '0 0 5px 5px'};
-//     border-radius: ${props.first && '5px 5px 0 0'};
-//     &:hover {
-//       transform: scale(1.025);
-//       border-radius: 5px;
-//       border: currentColor solid 2px;
-//       border-top: currentColor solid 1px;
-//     };
-//   `};
-// `;
-
 const RightSide = styled.div`
-  width: fit-content;
-  ${(props) => !props.searchClosed && css`
-    width: 100%;
-  `};
   display: flex;
   align-items: flex-end;
   margin-left: 1em;
+  width: fit-content;
+  padding-top: 0.25em;
+  padding-bottom: 0.25em;
+  ${(props) => !props.searchClosed && css`
+    width: 100%;
+  `};
 
-  @media (min-width: 700px) {
-    margin-left: 0px;
+  @media (min-width: 50rem) {
+    margin-left: 0.25em;
+    width: 100%;
+    padding-top: 0.75em;
     ${(props) => props.searchClosed && css`
       width: fit-content;
-      width: 25px;
-  `};
+      padding-top: 0.25em;
+    `};
   };
 
-  @media (min-width: 1000px) {
-    width: 150px;
+  @media (min-width: 57rem) {
+    width: 10em;
+    padding-top: 0.25em;
+    ${(props) => props.searchClosed && css`
+      width: fit-content;
+    `};
   };
 
-  @media (min-width: 1082px) {
-    width: fit-content;
+  @media (min-width: 65em) {
+    width: 11em;
   };
 
+  @media (min-width: 69em) {
+    width: initial;
+  };
 `;
-
-// @media (min-width: 700px) {
-//   width: 150px;
-//   margin-left: 0px;
-//   ${(props) => props.searchClosed && css`
-//     margin-left: 0px;
-//     width: fit-content;
-// `};
 
 // positioning is a little off - change background color to see
 const CollapsedNav = styled.button`
-  @media (min-width: 700px) {
-    display: none;
-  };
-  width: 25px;
   border: none;
-  background-color: ${(props) => props.theme.submitButtonHoverFont};
+  border-radius: 5px;
+  height: 2em;
+  width: 2.5em;
+  aspect-ratio: 1/1;
+  background-color: transparent;
   align-self: center;
-  padding-left: 0;
-  padding-right: 0px;
+  padding: 0.25em;
   text-align: center;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-end;
-  height: 20px;
+  justify-content: space-between;
+  align-items: center;
   margin-left: 1.0em;
-
+  color: ${(props) => props.theme.navFontColor};
+  opacity: 1.0;
+  &:hover {
+    opacity: 1.0;
+    color: ${(props) => props.theme.navActiveFontColor};
+  };
   ${(props) => props.searchClosed && css`
-    margin-left: 0px;
+    margin-left: 0.5em;
   `};
 
+  @media (min-width: 50rem) {
+    display: none;
+  };
 `;
-// margin-left: 2px;
+//color: ${(props) => props.theme.secondaryBackgroundColor};
 
 // TO-DO: add animation to make expanding nav smooth
 // TO-DO: collapse nav when click away
@@ -307,19 +277,14 @@ const ExpandedNav = styled.div`
   top: 40px;
   flex-direction: column;
   margin-top: 0.5%;
-  border-bottom: black solid 1px;
+  border-bottom: ${(props) => props.theme.fontColor} solid 1px;
 `;
-// width: calc(250px + 0.5vw);
-// border-radius: 5px;
 
 const IconBar = styled.span`
   display: block;
-  width: 25px;
-  height: 4px;
-  margin: 2px 0px;
-  background-color: ${(props) => props.theme.submitButtonHover};
+  width: 2em;
+  height: 0.25em;
+  background-color: currentColor;
 `;
-
-// text-indent: -999px;
 
 export default NavBar;
