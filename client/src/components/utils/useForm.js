a5eb98283574b52e72f58434a59dce5e1b22de91
Caroline Peake <peake.caroline@gmail.com>
2023-07-25 15:38:43 -0700

    create useForm hook

--- client/src/components/utils/useForm.js
+++ client/src/components/utils/useForm.js
@@ -2,21 +2,21 @@ import { useState } from 'react';
 
 // might need to initialize formState
 
-const useForm = (initialFormState = {}, onSubmit) => {
+const useForm = (onSubmit, initialFormState = {}) => {
   const [formState, setFormState] = useState(initialFormState);
   const [errors, setErrors] = useState([]);
-  const [validInput, setValidInput] = useState(false);
+  // const [validInput, setValidInput] = useState(false);
 
   const handleInputChange = (e) => {
-    setFormState({ ...formState, [e.target.name]: e.target.value });
-  }
+    const { name, value } = e.target;
+    setFormState({ ...formState, [name]: value });
+  };
 
   const validateInput = () => {
-    let errors = [];
+    const workingErrors = [];
     // could use reduce
     let valid = true;
 
-    // if (name === '' || email === '' || body === '') {
     // could make function just for single field
     const validateRequiredFields = (values) => {
       for (let i = 0; i < values.length; i++) {
@@ -25,55 +25,63 @@ const useForm = (initialFormState = {}, onSubmit) => {
         }
       }
       return true;
-    }
+    };
 
     const requiredFields = [formState.name, formState.email, formState.body];
+
     if (!validateRequiredFields(requiredFields)) {
-      let error = {
+      const error = {
         message: 'Not all mandatory fields have been provided.',
         id: '01',
       };
-      errors.push(error);
+      workingErrors.push(error);
       valid = false;
     }
 
     const validateEmail = (emailName) => {
       const regex = /\S+@\S+\.\S+/;
       return regex.test(emailName);
-    }
+    };
 
     if (!validateEmail(formState.email)) {
-      let error = {
-        message: 'Email is not in the correct email format.',
+      const error = {
+        message: 'Email is not in the correct format.',
         id: '02',
-      }
-      errors.push(error);
+      };
+      workingErrors.push(error);
       valid = false;
     }
 
-
-
-
-
-    setValidInput(valid);
-    setErrors(errors);
+    // setValidInput(valid);
+    setErrors(workingErrors);
     return valid;
-  }
+  };
 
   const resetForm = () => {
     setFormState(initialFormState);
     setErrors([]);
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log('formState: ', formState);
     if (!validateInput()) {
+      console.log('formm validation failed');
       return;
     }
-    onSubmit?.(formState);
-  }
+    // onSubmit?.(formState);
+    onSubmit(formState);
+    resetForm();
+  };
 
-  return [ formState, errors, handleInputChange, resetForm, handleSubmit ];
+  return [
+    formState,
+    errors,
+    handleInputChange,
+    resetForm,
+    // validateInput,
+    handleSubmit,
+  ];
 };
 
 export default useForm;
