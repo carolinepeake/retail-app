22a32244ed4941b86913dfb2df56004b983b5869
Caroline Peake <peake.caroline@gmail.com>
2023-07-25 15:37:41 -0700

    refactor characteristics to remove Product Characteristics heading and map single radio button component

--- client/src/components/RatingsAndReviews/AddRev/Characteristics.jsx
+++ client/src/components/RatingsAndReviews/AddRev/Characteristics.jsx
@@ -1,134 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import RadioButton from './RadioButton';
 
-function Characteristics({ revMeta, charVal, setCharVal, charObj, setCharObj }) {
-  const handleCharChange = function handleCharChange(numInput, curChar) {
-    const numVal = parseInt(numInput, 10);
-    if (curChar === 'Size') {
-      setCharObj((prevCharObj) => ({ ...prevCharObj, 135232: numVal }));
-      if (numVal === 1) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Size: 'A size too small', sizeNum: numVal }));
-      } else if (numVal === 2) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Size: '1/2 size too small', sizeNum: numVal }));
-      } else if (numVal === 3) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Size: 'Perfect', sizeNum: numVal }));
-      } else if (numVal === 4) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Size: '1/2 size too big', sizeNum: numVal }));
-      } else if (numVal === 5) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Size: 'A size too wide', sizeNum: numVal }));
-      }
-    }
+function Characteristics({
+  // id,
+  name,
+  characteristic,
+  handleInputChange,
+  inputState,
+}) {
+  // const [productCharacteristics, setProductCharacteristics] = useState({});
 
-    if (curChar === 'Width') {
-      setCharObj((prevCharObj) => ({ ...prevCharObj, 135233: numVal }));
-      if (numVal === 1) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Width: 'Too narrow', widthNum: numVal }));
-      } else if (numVal === 2) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Width: 'Slightly narrow', widthNum: numVal }));
-      } else if (numVal === 3) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Width: 'Perfect', widthNum: numVal }));
-      } else if (numVal === 4) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Width: 'Slightly wide', widthNum: numVal }));
-      } else if (numVal === 5) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Width: 'Too wide', widthNum: numVal }));
-      }
-    }
+  console.log('name: ', name, 'characteristic: ', characteristic, 'handleInputChange: ', handleInputChange, 'inputState: ', inputState);
 
-    if (curChar === 'Comfort') {
-      setCharObj((prevCharObj) => ({ ...prevCharObj, 135230: numVal }));
-      if (numVal === 1) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Comfort: 'Uncomfortable', comfortNum: numVal }));
-      } else if (numVal === 2) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Comfort: 'Slightly comfortable', comfortNum: numVal }));
-      } else if (numVal === 3) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Comfort: 'Ok', comfortNum: numVal }));
-      } else if (numVal === 4) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Comfort: 'Comfortable', comfortNum: numVal }));
-      } else if (numVal === 5) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Comfort: 'Perfect', comfortNum: numVal }));
-      }
-    }
-
-    if (curChar === 'Quality') {
-      setCharObj((prevCharObj) => ({ ...prevCharObj, 135231: numVal }));
-      if (numVal === 1) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Quality: 'Poor', qualityNum: numVal }));
-      } else if (numVal === 2) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Quality: 'Below Average', qualityNum: numVal }));
-      } else if (numVal === 3) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Quality: 'What I expected', qualityNum: numVal }));
-      } else if (numVal === 4) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Quality: 'Pretty Great', qualityNum: numVal }));
-      } else if (numVal === 5) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Quality: 'Perfect', qualityNum: numVal }));
-      }
-    }
-
-    if (curChar === 'Length') {
-      setCharObj((prevCharObj) => ({ ...prevCharObj, 135229: numVal }));
-      if (numVal === 1) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Length: 'Runs Short', lengthNum: numVal }));
-      } else if (numVal === 2) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Length: 'Runs slightly short', lengthNum: numVal }));
-      } else if (numVal === 3) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Length: 'Perfect', lengthNum: numVal }));
-      } else if (numVal === 4) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Length: 'Runs slightly long', lengthNum: numVal }));
-      } else if (numVal === 5) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Length: 'Runs long', lengthNum: numVal }));
-      }
-    }
-
-    if (curChar === 'Fit') {
-      setCharObj((prevCharObj) => ({ ...prevCharObj, 135228: numVal }));
-      if (numVal === 1) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Fit: 'Runs Short', fitNum: numVal }));
-      } else if (numVal === 2) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Fit: 'Runs slightly short', fitNum: numVal }));
-      } else if (numVal === 3) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Fit: 'Perfect', fitNum: numVal }));
-      } else if (numVal === 4) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Fit: 'Runs slightly long', fitNum: numVal }));
-      } else if (numVal === 5) {
-        setCharVal((prevCharVal) => ({ ...prevCharVal, Fit: 'Runs long', fitNum: numVal }));
-      }
-    }
+  // wrap in useCallback
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const updatedCharacteristics = { ...inputState, [name]: value };
+    const event = {
+      target: {
+        name: 'characteristics',
+        value: updatedCharacteristics,
+      },
+    };
+    handleInputChange(event);
+    // setProductCharacteristics({ ...productCharacteristics, [e.target.name]: e.target.value });
   };
 
-  return (
-    <div>
-      <div>
-        Product characteristics*
-      </div>
+  // const { id } = CHARACTERISTICS[characteristic];
+  // const characteristic = CHARACTERISTICS[id];
 
-      {Object.keys(revMeta.characteristics).map((char) => (
-        <CharDiv key={char}>
-          <div>
-            {char}
-            :&nbsp;
-            {
-            !charVal[char]
-              ? 'none selected'
-              : charVal[char]
-            }
-          </div>
-          <RadioButtonsContainer onChange={(event) => handleCharChange(event.target.value, char)}>
-            <input type="radio" value={1} name={char} />
-            {/* remember to make above input required */}
-            1
-            <input type="radio" value={2} name={char} />
-            2
-            <input type="radio" value={3} name={char} />
-            3
-            <input type="radio" value={4} name={char} />
-            4
-            <input type="radio" value={5} name={char} />
-            5
-          </RadioButtonsContainer>
-        </CharDiv>
-      ))}
-    </div>
+  const id = '' + characteristic.id;
+  const selectedValue = inputState[id];
+  let selectedIndex;
+  let selectedLabel;
+  if (selectedValue) {
+    selectedIndex = Number(selectedValue) - 1;
+    selectedLabel = characteristic.values[selectedIndex].label;
+  };
+
+  console.log('selectedValue: ', selectedValue, 'selectedIndex: ', selectedIndex, 'inputState: ', inputState, 'id: ', id);
+  // const selectedLabel = characteristic?.values?[Number(selectedValue) - 1]?.label;
+
+  return (
+    <>
+      <Subheader>
+        {name}
+        {/* : */}
+        <Required>*</Required>
+        {selectedValue
+          ? <SelectedLabel>
+              {/* &nbsp; */}
+              {selectedLabel}
+            </SelectedLabel>
+          : <Placeholder>
+              {/* &nbsp; */}
+              None Selected
+            </Placeholder>
+        }
+      </Subheader>
+
+      <RadioButtonsContainer>
+
+        {characteristic?.values?.map(({ value, label }) => (
+          <RadioButton
+            required
+            value={value}
+            label={label}
+            name={id}
+            handleChange={handleChange}
+            checked={Number(selectedValue) === value}
+          />
+        ))}
+      </RadioButtonsContainer>
+    </>
   );
 }
 
@@ -136,17 +82,39 @@ Characteristics.propTypes = {
   revMeta: PropTypes.shape({
     characteristics: PropTypes.shape({}),
   }).isRequired,
-  charVal: PropTypes.shape({}).isRequired,
-  setCharVal: PropTypes.func.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  characteristics: PropTypes.shape({}).isRequired,
 };
 
 export default Characteristics;
 
-const CharDiv = styled.div`
-  margin-left: 1rem;
+const Subheader = styled.div`
+  margin-bottom: 0.75em;
+`;
+
+const Required = styled.sup`
+  color: ${(props) => props.theme.formError}
+`;
+
+const Placeholder = styled.span`
+  color: ${(props) => props.theme.inputPlaceholder};
+  display: block;
   margin-top: 0.5em;
+  font-size: 0.83em
+  /* margin-left: 0.5em;
+  color: #555;
+  font-style: oblique; */
+`;
+
+const SelectedLabel = styled(Placeholder)`
+  color: ${(props) => props.theme.secondaryFontColor};
+  font-weight: 400;
 `;
 
 const RadioButtonsContainer = styled.div`
-  margin-top: 0.25em;
+  margin: 0 5%;
+  display: flex;
+  position: relative;
+  border-top: 1px ${(props) => props.theme.secondaryFontColor} solid;
+  justify-content: space-between;
 `;
