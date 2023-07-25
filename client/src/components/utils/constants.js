bf6564a64e9479f1c8d7d2e1692f58db0aefa897
Caroline Peake <peake.caroline@gmail.com>
2023-07-25 15:32:31 -0700

    separate review product characteristics, id, and label into constants file

---
+++ client/src/components/utils/constants.js
@@ -0,0 +1,239 @@
+const CHARACTERISTICS = {
+  // 135232: {
+  //   label: 'Size',
+  //   values: {
+  //     1: 'A size too small',
+  //     2: '1/2 size too small',
+  //     3: 'Perfect',
+  //     4: '1/2 size too big',
+  //     5: 'A size too big',
+  //   },
+  // },
+  Size: {
+    id: 135232,
+    values: [
+      {
+        value: 1,
+        label: 'A size too small',
+      },
+      {
+        value: 2,
+        label: '1/2 size too small',
+      },
+      {
+        value: 3,
+        label: 'Perfect',
+      },
+      {
+        value: 4,
+        label: '1/2 size too big',
+      },
+      {
+        value: 5,
+        label: 'A size too big',
+      },
+    ],
+  },
+  // 135233: {
+  //   label: 'Width',
+  //   values: {
+  //     1: 'Too narrow',
+  //     2: 'Slightly narrow',
+  //     3: 'Perfect',
+  //     4: 'Slightly wide',
+  //     5: 'Too wide',
+  //   },
+  // },
+  Width: {
+    id: 135233,
+    values: [
+      {
+        value: 1,
+        label: 'Too narrow',
+      },
+      {
+        value: 2,
+        label: 'Slightly narrow',
+      },
+      {
+        value: 3,
+        label: 'Perfect',
+      },
+      {
+        value: 4,
+        label: 'Slightly wide',
+      },
+      {
+        value: 5,
+        label: 'Too wide',
+      },
+    ],
+  },
+  // 135230: {
+  //   label: 'Comfort',
+  //   values: {
+  //     1: 'Uncomfortable',
+  //     2: 'Slightly comfortable',
+  //     3: 'Ok',
+  //     4: 'Comfortable',
+  //     5: 'Perfect',
+  //   },
+  // },
+  Comfort: {
+    id: 135230,
+    values: [
+      {
+        value: 1,
+        label: 'Uncomfortable',
+      },
+      {
+        value: 2,
+        label: 'Slightly comfortable',
+      },
+      {
+        value: 3,
+        label: 'Ok',
+      },
+      {
+        value: 4,
+        label: 'Comfortable',
+      },
+      {
+        value: 5,
+        label: 'Perfect',
+      },
+    ],
+  },
+  // 135231: {
+  //   label: 'Quality',
+  //   values: {
+  //     1: 'Poor',
+  //     2: 'Below Average',
+  //     3: 'What I expected',
+  //     4: 'Pretty Great',
+  //     5: 'Perfect',
+  //   },
+  // },
+  Quality: {
+    id: 135231,
+    values: [
+      {
+        value: 1,
+        label: 'Poor',
+      },
+      {
+        value: 2,
+        label: 'Below Average',
+      },
+      {
+        value: 3,
+        label: 'What I expected',
+      },
+      {
+        value: 4,
+        label: 'Pretty Great',
+      },
+      {
+        value: 5,
+        label: 'Perfect',
+      },
+    ],
+  },
+  // 135229: {
+  //   label: 'Length',
+  //   values: {
+  //     1: 'Runs Short',
+  //     2: 'Runs slightly short',
+  //     3: 'Perfect',
+  //     4: 'Runs slightly long',
+  //     5: 'Runs long',
+  //   },
+  // },
+  Length: {
+    id: 135229,
+    values: [
+      {
+        value: 1,
+        label: 'Runs Short',
+      },
+      {
+        value: 2,
+        label: 'Runs slightly short',
+      },
+      {
+        value: 3,
+        label: 'Perfect',
+      },
+      {
+        value: 4,
+        label: 'Runs slightly long',
+      },
+      {
+        value: 5,
+        label: 'Runs long',
+      },
+    ],
+  },
+  // 135228: {
+  //   label: 'Fit',
+  //   values: {
+  //     1: 'Runs tight',
+  //     2: 'Runs slightly tight',
+  //     3: 'Perfect',
+  //     4: 'Runs slightly loose',
+  //     5: 'Runs loose',
+  //   },
+  // },
+  // 135228: {
+  //   label: 'Fit',
+  //   values: [
+  //     {
+  //       value: 1,
+  //       label: 'Runs tight',
+  //     },
+  //     {
+  //       value: 2,
+  //       label: 'Runs slightly tight',
+  //     },
+  //     {
+  //       value: 3,
+  //       label: 'Perfect',
+  //     },
+  //     {
+  //       value: 4,
+  //       label: 'Runs slightly loose',
+  //     },
+  //     {
+  //       value: 5,
+  //       label: 'Runs loose',
+  //     },
+  //   ],
+  // },
+  Fit: {
+    id: 135228,
+    values: [
+      {
+        value: 1,
+        label: 'Runs tight',
+      },
+      {
+        value: 2,
+        label: 'Runs slightly tight',
+      },
+      {
+        value: 3,
+        label: 'Perfect',
+      },
+      {
+        value: 4,
+        label: 'Runs slightly loose',
+      },
+      {
+        value: 5,
+        label: 'Runs loose',
+      },
+    ],
+  },
+};
+
+export default CHARACTERISTICS;
