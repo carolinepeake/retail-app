01e86a9f39bb9032b2c48f8e14c418ab2b0690f4
Caroline Peake <peake.caroline@gmail.com>
2023-07-27 12:40:53 -0700

    make a reusable form input component. not yet used

---
+++ client/src/components/reusable/Form/Input.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import axios from 'axios';
+import { Button } from '../Button';
+import { useGlobalContext } from '../../../contexts/GlobalStore';
+// import AddPhotos from '../../RatingsAndReviews/AddRev/AddPhotos';
+import AddPhotos2 from './AddPhotos2';
+import Modal from '../Modal';
+import useForm from '../../utils/useForm';
+
+function FormInput({
+  id, label, required, disclaimer, ...props
+}) {
+  return (
+    <>
+      <FormLabel htmlFor={id}>
+        {label}
+        {required && <Required>*</Required>}
+      </FormLabel>
+      <StyledFormInput
+        id={id}
+        required={required}
+        {...props}
+        value={formState[name].value}
+        type={props.type}
+        name={props.name}
+        error={props.errors[name]} // { error: '', message '' }
+        onChange={handleInputChange}
+        validation={props.validationSchema[name]} // [ {rule: '', test: } ]
+        placeholder={props.placeholder}
+        maxLength={props.maxLength}
+      />
+      {disclaimer && (
+      <Disclaimer>
+        {disclaimer}
+      </Disclaimer>
+      )}
+      {error && <Error>{error.message}</Error>}
+    </>
+  );
+}
+
+const FormLabel = styled.label`
+  font-size: 1rem;
+  cursor: initial;
+`;
+
+const StyledFormInput = styled.input`
+  display: block;
+  width: 100%;
+  padding: 0.5em;
+  margin-top: 0.25em;
+  border: currentColor solid thin;
+  cursor: initial;
+  font-family: inherit;
+  font-size: ${(props) => props.theme.input};
+  color: ${(props) => props.theme.fontColor};
+  background-color: ${(props) => props.theme.backgroundColor};
+
+  ::placeholder {
+    color: ${(props) => props.theme.inputPlaceholder};
+  }
+
+  &:focus {
+    background-color: ${(props) => props.theme.navBgColor};
+  }
+
+  ::label {
+    font-size: 1rem;
+    color: ${(props) => props.theme.fontColor};
+  }
+
+  /* ::error {
+  } ? */
+`;
+
+const Required = styled.sup`
+  color: ${(props) => props.theme.formError};
+`;
+
+const Disclaimer = styled.h5`
+  font-style: oblique;
+`;
+
+const Error = styled.div`
+  color: ${(props) => props.theme.formError};
+  font-style: oblique;
+`;
+
+export default FormInput;
