87ba31f45e75e41a0f4ecdb4487186762bc11980
Caroline Peake <peake.caroline@gmail.com>
2023-07-27 12:46:35 -0700

    create useModal hook

--- client/src/components/utils/useModal.js
+++ client/src/components/utils/useModal.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 export default function useModal() {
   const [showModal, setShowModal] = useState(false);
+  console.log('toggling modal in useModal');
 
   function toggleModal() {
     setShowModal((prev) => !prev);
