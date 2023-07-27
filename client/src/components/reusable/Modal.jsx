f7ae6e951e616f8381b5d07c83f084ca4de1d68c
Caroline Peake <peake.caroline@gmail.com>
2023-07-27 12:48:19 -0700

    create reusable Modal component

--- client/src/components/reusable/Modal.jsx
+++ client/src/components/reusable/Modal.jsx
@@ -1,117 +1,46 @@
 import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-// import axios from 'axios';
 import { Button } from './Button';
-// import { useGlobalContext } from '../../../contexts/GlobalStore';
-// import useModal from '../utils/useModal';
 
-// function ModalHeader({ children }) {
-//   return (
-//     <Header>
-//       {children}
-//     </Header>
-//   );
-// }
-
-// const Header = styled.header`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: flex-start;
-//   width: 100%;
-//   @media (min-width: 40rem) {
-//     width: 90%;
-//   };
-
-//   @media (min-width: 50rem) {
-//     width: 80%;
-//   };
-//   margin-bottom: 0.5rem;
-// `;
-
-// function ModalBody({ children }) {
-//   return (
-//     <ModalBody>
-//       {children}
-//     </ModalBody>
-//   );
-// }
-
-// function ModalFooter({ children }) {
-//   return (
-//     <Footer>
-//       {children}
-//     </Footer>
-//   );
-// }
-
-// const Footer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   padding-top: 1.5em;
-//   width: 100%;
-
-//   @media (min-width: 40rem) {
-//     width: 90%;
-//   };
-
-//   @media (min-width: 50rem) {
-//     width: 80%;
-//   };
-
-//   @media (min-width: 600px) {
-//     flex-direction: row;
-//     column-gap: 2rem;
-//     justify-content: space-evenly;
-//     align-items: center;
-//   };
-// `;
-
-function Modal({ showModal, closeModal, children }) {
+function Modal({
+  closeModal,
+  children,
+}) {
   function clickOutsideModal(event) {
-    if (event.target.id === 'background') {
+    if (event.target.id === 'backgroundDiv') {
       closeModal();
     }
   }
   // click outside done another way
   // const inputRef = useRef(null);
-  const modalRef = useRef(null);
+  // const modalRef = useRef(null);
 
-  const onClickOutside = (e) => {
-    const element = e.target;
-    if (modalRef.current && !modalRef.current.contains(element)) {
-      e.preventDefault();
-      e.stopPropagation();
-      closeModal();
-    }
-  };
-
-  useEffect(() => {
-    // inputRef.current.focus();
-    document.body.addEventListener('click', onClickOutside);
-    return () => document.removeEventListener('click', onClickOutside);
-  }, []);
-
-  // const onChange = (e) => {
-  //   setValue(e.target.value);
+  // const onClickOutside = (e) => {
+  //   const element = e.target;
+  //   if (modalRef.current && !modalRef.current.contains(element)) {
+  //     e.preventDefault();
+  //     e.stopPropagation();
+  //     closeModal();
+  //   }
   // };
 
-  // const onSub = (e) => {
-  //   e.preventDefault();
-  //   onSubmit(value);
-  //   onClose();
-  // };
+  // useEffect(() => {
+  //   // inputRef.current.focus();
+  //   document.body.addEventListener('click', onClickOutside);
+  //   return () => document.removeEventListener('click', onClickOutside);
+  // }, []);
 
-  return showModal ? (
+  return (
     <ModalBackground
-      id="background"
+      id="backgroundDiv"
       onClick={(event) => clickOutsideModal(event)}
     >
       <ModalContainer
-        ref={modalRef}
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
+        // ref={modalRef}
+        // onClick={(e) => {
+        //   e.stopPropagation();
+        // }}
       >
         <Button close onClick={closeModal}>
           &#x2715;
@@ -119,12 +48,11 @@ function Modal({ showModal, closeModal, children }) {
         {children}
       </ModalContainer>
     </ModalBackground>
-  ) : null;
+  );
 }
 
 Modal.propTypes = {
   closeModal: PropTypes.func.isRequired,
-  showModal: PropTypes.bool.isRequired,
 };
 
 const ModalBackground = styled.div`
@@ -155,17 +83,19 @@ const ModalContainer = styled.div`
   background-color: ${(props) => props.theme.backgroundColor};
   overflow: auto;
   position: relative;
+  max-width: 40em;
 
   @media (min-width: 40rem) {
-    width: 70vw;
+   /* width: 70vw; */
     border: 1px solid;
     max-height: 90vh;
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    padding: 3em;
   }
 
   @media (min-width: 50rem) {
     max-height: 80vh;
-    width: 60vw;
+   /* width: 60vw; */
     z-index: 21;
     top: 1.5rem;
   }
