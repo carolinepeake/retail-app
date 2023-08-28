c518841502cf139dcb8ee6ade80a17940827ed73
Caroline Peake <peake.caroline@gmail.com>
2023-08-27 19:36:31 -0700

    remove underline below section header

--- client/src/components/SectionHeader.jsx
+++ client/src/components/SectionHeader.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const SectionHeading = styled.div`
-  border-bottom: grey solid thin;
+ /* border-bottom: grey solid thin; */
   font-size:  1.5rem;
   padding-bottom: 0.5em;
   margin-bottom: 0.5em;
