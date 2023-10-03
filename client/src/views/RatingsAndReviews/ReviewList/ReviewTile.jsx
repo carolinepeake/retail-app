d757fe669dbe51ec603394a2e53340bae9e548a1
Caroline Peake <peake.caroline@gmail.com>
2023-10-03 00:08:01 -0700

    refine css for review tile

--- client/src/views/RatingsAndReviews/ReviewList/ReviewTile.jsx
+++ client/src/views/RatingsAndReviews/ReviewList/ReviewTile.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import HelpfulReport from './HelpfulReport';
 import StarCount from './StarCount';
+import Stars from '../../RelatedItems/RelatedList/Stars';
 import ExpandedImageModal from '../../../components/ExpandedImageModal';
 import { formatDate } from '../../../utils/getFormat';
 
@@ -19,11 +20,14 @@ function ReviewTile({ review }) {
     <Container>
 
       <StarsDateName>
-        <StarCount rating={review.rating} />
+        {/* <StarCount rating={review.rating} /> */}
+        <StarsContainer>
+          <Stars rating={review.rating} />
+        </StarsContainer>
         <DateName>
           <ReviewerName>
-            {`${review.reviewer_name},`}
-            &nbsp;
+            {`${review.reviewer_name}`}
+            {/* &nbsp; */}
           </ReviewerName>
           <Date>
             {formatDate(review.date)}
@@ -46,16 +50,25 @@ function ReviewTile({ review }) {
       </Recommend>
       )}
 
+      {review.photos.length > 0
+      && (
       <PhotosDiv>
         {review.photos.map((photo) => (
           <RevImg
             key={photo.id}
+            height="4 rem"
+            width="4 rem"
             alt=""
             src={photo.url}
             onClick={handlePhotoClick}
           />
         ))}
       </PhotosDiv>
+      )}
+
+      {/* <ReviewerName>
+        {review.reviewer_name}
+      </ReviewerName> */}
 
       {review.response
         && (
@@ -103,41 +116,84 @@ ReviewTile.propTypes = {
 export default ReviewTile;
 
 const Container = styled.div`
-  border-bottom: currentColor solid thin;
+  padding: 1rem;
   overflow-wrap: anywhere;
-  background-color: ${(props) => props.theme.backgroundColor};
-  padding: 0.5em 0 0.5em 0.5em;
-  margin-top: 0.5em;
-  margin-right: 0.5em;
+  background-color: rgb(245,245,245);
+  border-radius: 5px;
+  margin-top: 0.5rem;
+  line-height: 1.5rem;
+
+  @media (min-width: 400px) {
+    padding: 1rem 0 1rem 0.5rem;
+    margin-right: 0.5rem;
+    margin-top: 0;
+    background-color: ${(props) => props.theme.backgroundColor};
+    border-bottom: ${(props) => props.theme.lightBorder};
+    border-radius: 0;
+
+    &&:first-child {
+      border-top: ${(props) => props.theme.lightBorder};
+    }
+  }
 `;
 
 const StarsDateName = styled.div`
   display: flex;
-  justify-content: space-between;
-  margin-top: 0.5em;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1.0rem;
+  align-items: flex-start;
+  margin-bottom: 0;
+
+  @media (min-width: 400px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+  }
 `;
 
 const DateName = styled.h5`
   display: flex;
-  justify-content: flex-end;
-  font-size: 1em;
+  font-size: ${(props) => props.theme.tertiary};
   padding-top: 0;
+  gap: 0.5em;
+  width: 100%;
+  color: ${(props) => props.theme.minorFontColor};
+  margin: 0;
+  order: -1;
+  justify-content: space-between;
+
+
+  @media (min-width: 400px) {
+    order: 0;
+    justify-content: flex-end;
+    width: auto;
+  }
+`;
+
+const StarsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 1.5rem;
 `;
 
 const ReviewerName = styled.div`
-  padding-right: 1em;
+  color: ${(props) => props.theme.minorFontColor};
 
-  @media (min-width: 600px) AND (max-width: 700 px) {
-    padding-right: 0.5em;
+  @media (min-width: 400px) {
+    &:after {
+      content: ',';
+    }
   }
 `;
 
 const Date = styled.div`
-  font-weight: 400;
+  /* font-weight: 300; */
 `;
 
 const Response = styled.div`
-  padding: 1em;
+  padding: 1rem;
   background-color: lightgrey;
 `;
 
@@ -145,26 +201,34 @@ const PhotosDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: flex-start;
+  gap: 1rem;
+  margin-bottom: 1rem;
+  padding: 0.25rem 0;
+  line-height: 1.5rem;
 `;
 
 const RevImg = styled.img`
-  max-height: 8em;
-  max-width: 8em;
-  padding: .5em;
+  height: 4rem;
+  width: 4rem;
+  width: 100%;
+  height: auto;
+  max-height: 8rem;
+  max-width: 8rem;
   object-fit: scale-down;
   cursor: pointer;
 `;
 
 const Summary = styled.h3`
   display: flex;
+/*  margin: 1rem 0; */
 `;
 
 const Body = styled.p`
   display: flex;
   font-size: 1.0em;
-  margin: 1em 0;
+  margin: 1rem 0;
 `;
 
 const Recommend = styled.h4`
-  margin-bottom: 1em;
+  margin-bottom: 1rem;
 `;
