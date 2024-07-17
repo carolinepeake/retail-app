3dd8bd2562e41908504d503ef3c1cd2668c0d146
Caroline Peake <peake.caroline@gmail.com>
2024-07-17 03:33:40 -0700

    refactor products controller to remove if clause under try block

--- server/controllers/Products.js
+++ server/controllers/Products.js
@@ -1,76 +1,47 @@
 const axiosInstance = require('./axiosInstance');
 
-module.export = {
+module.exports = {
   getProducts: async (req, res, next) => {
+    const { page, count } = req.params;
     try {
-      const products = await axiosInstance.get('/products');
-      if (products) {
-        res.send(products);
-        next();
-      } else {
-        res.status(404).error({ message: 'atellier api route incorrect' });
-      }
+      const products = await axiosInstance.get(`/products/?page=${page}&&count=${count}`);
+      res.send(products.data);
+      next();
     } catch (error) {
-      res.status(500).error({ message: error.message });
+      res.status(500).send({ message: error.message });
     }
   },
 
   getProductById: async (req, res, next) => {
     const id = req.params.product_id;
     try {
-      const product = await axiosInstance.get(`/product/${id}`);
-      if (product) {
-        res.send(product);
-        next();
-      } else {
-      // not sure about this
-        res.status(404).send({ message: 'atellier api route incorrect' });
-      }
+      const product = await axiosInstance.get(`/products/${id}`);
+      res.send(product.data);
+      next();
     } catch (err) {
+      console.log('error fetching product info from API: ', err);
       res.status(500).send({ message: err.message });
     }
   },
 
-  getStyles: async (req, res, next) => {
-    try {
-      const styles = await axiosInstance.get('/products/styles');
-      if (styles) {
-        res.send(styles);
-        next();
-      } else {
-        res.status(404).error({ message: 'atellier api route incorrect' });
-      }
-    } catch (error) {
-      res.status(500).error({ message: error.message });
-    }
-  },
-
-  getStylesById: async (next, req, res) => {
+  getProductStyles: async (req, res, next) => {
     const id = req.params.product_id;
     try {
-      const style = await axiosInstance.get(`/products/${id}/styles`);
-      if (style) {
-        res.send(style);
-        next();
-      } else {
-        res.status(404).error({ message: 'atellier api route incorrect' });
-      }
+      const styles = await axiosInstance.get(`/products/${id}/styles`);
+      res.send(styles.data);
+      next();
     } catch (error) {
-      res.status(500).error({ message: error.message });
+      res.status(500).send({ message: error.message });
     }
   },
 
+  // TODO: save in related data obj with product id as key or add to productId data obj
   getRelated: async (req, res, next) => {
-    const id = req.params.productID;
+    const id = req.params.product_id;
     try {
       const related = await axiosInstance.get(`/products/${id}/related`);
-      if (related) {
-        res.send(related);
-        next();
-      } else {
-      // not sure about this
-        res.status(404).send({ message: 'atellier api route incorrect' });
-      }
+      res.send(related.data);
+      next();
     } catch (err) {
       res.status(500).send({ message: err.message });
     }
